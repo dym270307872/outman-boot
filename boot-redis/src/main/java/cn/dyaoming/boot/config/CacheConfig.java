cb1d34fecc7f09b86d02c68a80c52fccc17171ef
dym270307872 <270307872@qq.com>
2019-04-18 15:22:02 +0800

    Initial commit

---
+++ boot-redis/src/main/java/cn/dyaoming/boot/config/CacheConfig.java
@@ -0,0 +1,92 @@
+/*
+package com.jege.spring.boot.config;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.interceptor.KeyGenerator;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.cache.RedisCacheWriter;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+
+import java.lang.reflect.Method;
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+//@Configuration
+public class CacheConfig {
+
+    @Bean
+    public CacheManager cacheManager(RedisConnectionFactory redisConnectionFactory) {
+        return new RedisCacheManager(
+                RedisCacheWriter.nonLockingRedisCacheWriter(redisConnectionFactory),
+                this.getRedisCacheConfigurationWithTtl(30 * 60), // 默认策略，未配置的 key 会使用这个
+                this.getRedisCacheConfigurationMap() // 指定 key 策略
+        );
+    }
+
+    private Map<String, RedisCacheConfiguration> getRedisCacheConfigurationMap() {
+        Map<String, RedisCacheConfiguration> redisCacheConfigurationMap = new HashMap<>();
+        //配置缓存名及过期时间
+        redisCacheConfigurationMap.put("default", this.getRedisCacheConfigurationWithTtl(24 * 60 * 60));
+        redisCacheConfigurationMap.put("publicInfo", this.getRedisCacheConfigurationWithTtl(3 * 60 * 60));
+        redisCacheConfigurationMap.put("userInfo", this.getRedisCacheConfigurationWithTtl(30 * 60));
+        redisCacheConfigurationMap.put("businessInfo", this.getRedisCacheConfigurationWithTtl(5 * 60));
+        return redisCacheConfigurationMap;
+    }
+
+    private RedisCacheConfiguration getRedisCacheConfigurationWithTtl(Integer seconds) {
+        Jackson2JsonRedisSerializer<Object> jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer<>(Object.class);
+        ObjectMapper om = new ObjectMapper();
+        om.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+        om.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
+        jackson2JsonRedisSerializer.setObjectMapper(om);
+
+        RedisCacheConfiguration redisCacheConfiguration = RedisCacheConfiguration.defaultCacheConfig();
+
+        redisCacheConfiguration = redisCacheConfiguration.serializeValuesWith(
+                RedisSerializationContext
+                        .SerializationPair
+                        .fromSerializer(jackson2JsonRedisSerializer)
+        ).entryTtl(Duration.ofSeconds(seconds));
+
+        return redisCacheConfiguration;
+    }
+
+
+    // 该值是 keyGenerator 方法的方法名称，如果Bean 指定了名称，则使用指定的名称
+    public static final String DEFAULT_KEY_GENERATOR = "keyGenerator";
+
+    // 定义缓存区，缓存区可以在配置时指定不同的过期时间，作为防止缓存雪崩的一个保护措施
+    public static final String COMMON = "COMMON";
+
+    @Bean
+
+    public KeyGenerator keyGenerator() {
+        return new KeyGenerator() {
+            @Override
+            public Object generate(Object target, Method method, Object... params) {
+                StringBuilder sb = new StringBuilder();
+                sb.append(target.getClass().getName());
+                sb.append("." + method.getName());
+                if (params == null || params.length == 0 || params[0] == null) {
+                    return null;
+                }
+                String join = String.join("&", Arrays.stream(params).map(Object::toString).collect(Collectors.toList()));
+                String format = String.format("%s{%s}", sb.toString(), join);
+                //log.info("缓存key：" + format);
+                return format;
+            }
+        };
+    }
+}
+*/
---
+++ boot-redis/src/main/java/cn/dyaoming/dao/RedisTemplateBatchImp.java
@@ -0,0 +1,371 @@
+/*package cn.dyaoming.dao;
+
+
+import cn.dyaoming.errors.AppDaoException;
+import cn.dyaoming.interfaces.CacheBatchInterface;
+import cn.dyaoming.interfaces.CacheInterface;
+import cn.dyaoming.utils.SerializeUtil;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.data.redis.connection.RedisConnection;
+import org.springframework.data.redis.core.*;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+
+*//**
+ * 类名称：RedisTemplateImp
+ * <p/>
+ * 类描述： 一个通过Spring RedisTemplate 处理数据缓存的基础接口 实现类。
+ * <p/>
+ * 创建时间：2017-02-12
+ * <p/>
+ * 创建人： DYM
+ * <p/>
+ * 修改人：无
+ * <p/>
+ * 修改时间：无
+ * <p/>
+ * 修改备注：无
+ * <p/>
+ * 版本：V1.0
+ *//*
+
+//@Component("cacheDao")
+public class RedisTemplateBatchImp implements CacheBatchInterface {
+
+	private static final Logger logger = LogManager.getLogger(RedisTemplateBatchImp.class);
+
+	@Autowired
+	private RedisTemplate redisTemplate;
+
+
+
+
+	*//**
+	 * 功能描述：获取 keys。
+	 * <p/>
+	 * 创建时间：2017-02-12
+	 * <p/>
+	 * 创建人： DYM
+	 * <p/>
+	 * 修改人：无
+	 * <p/>
+	 * 修改时间：无
+	 * <p/>
+	 * 修改备注：无
+	 *
+	 * @param pattern String类型 key的表达式，也可以使用通配符(*)
+	 * @return Collection<String>类型 返回结果
+	 *//*
+
+	@Override
+	public Collection<String> getKeys(String pattern) throws AppDaoException {
+		return redisTemplate.keys(pattern);
+	}
+
+
+
+
+
+
+	*//**
+	 * 功能描述：使用通配符批量删除缓存内容。
+	 * <p/>
+	 * 创建时间：2017-02-12
+	 * <p/>
+	 * 创建人： DYM
+	 * <p/>
+	 * 修改人：无
+	 * <p/>
+	 * 修改时间：无
+	 * <p/>
+	 * 修改备注：无
+	 *
+	 * @param keyPrefix String类型 键前缀
+	 * @param symbol    String类型 符号
+	 * @return boolean类型 返回结果
+	 *//*
+
+	@Override
+	public boolean deleteCacheData(String keyPrefix, String symbol) throws AppDaoException {
+		boolean rv = false;
+
+		try {
+			// 使用通配符 进行 查询所有Key
+			Set<String> keys = redisTemplate.keys(keyPrefix + symbol);
+
+			redisTemplate.delete(keys);
+
+			rv = true;
+
+		} catch(Exception e) {
+			logger.error("异常：deleteCacheData()方法出现异常，异常详细信息：" + e.getMessage() + "。");
+			throw new AppDaoException("删除缓存内容出现异常！", e);
+		}
+
+		return rv;
+	}
+
+
+
+	*//**
+	 * 功能描述：批量删除缓存内容。
+	 * <p/>
+	 * 创建时间：2017-02-12
+	 * <p/>
+	 * 创建人： DYM
+	 * <p/>
+	 * 修改人：无
+	 * <p/>
+	 * 修改时间：无
+	 * <p/>
+	 * 修改备注：无
+	 *
+	 * @param key List<String>类型 键
+	 * @return boolean类型 返回结果
+	 *//*
+
+	@Override
+	public boolean deleteCacheData(List<String> key) throws AppDaoException {
+		boolean rv = false;
+
+		try {
+			redisTemplate.delete(key);
+
+			rv = true;
+
+		} catch(Exception e) {
+			logger.error("异常：deleteCacheData()方法出现异常，异常详细信息：" + e.getMessage() + "。");
+			throw new AppDaoException("删除缓存内容出现异常！", e);
+		}
+
+		return rv;
+	}
+
+
+
+
+
+
+
+	*//**
+	 * 功能描述：获取缓存内容。
+	 * <p/>
+	 * 创建时间：2017-02-12
+	 * <p/>
+	 * 创建人： DYM
+	 * <p/>
+	 * 修改人：无
+	 * <p/>
+	 * 修改时间：无
+	 * <p/>
+	 * 修改备注：无
+	 *
+	 * @param key  String类型 键
+	 * @param type Class<T>类型 内容类型
+	 * @return T类型 返回结果
+	 *//*
+
+	@Override
+	public <T> T getCacheTData(String key, Class<T> type) throws AppDaoException {
+		if (StringUtils.isEmpty(key) || null == type) {
+			return null;
+		} else {
+			final String finalKey;
+			final Class<T> finalType = type;
+			if (key instanceof String) {
+				finalKey = key;
+			} else {
+				finalKey = key.toString();
+			}
+			final Object object = redisTemplate.execute(new RedisCallback<Object>() {
+				@Override
+				public Object doInRedis(RedisConnection connection) throws DataAccessException {
+					byte[] key = (finalKey).getBytes();
+					byte[] value = connection.get(key);
+					if (value == null) {
+						return null;
+					}
+					return SerializeUtil.unSerialize(value);
+				}
+			});
+			if (finalType != null && finalType.isInstance(object) && null != object) {
+				return (T) object;
+			} else {
+				return null;
+			}
+		}
+	}
+
+
+
+	*//**
+	 * 描述： redis设置Map
+	 * <p/>
+	 * 创建时间：2017-03-23
+	 * <p/>
+	 * 创建人： 董耀明
+	 * <p/>
+	 * 修改人：无
+	 * <p/>
+	 * 修改时间：无
+	 * <p/>
+	 * 修改备注：无
+	 * <p/>
+	 * 版本：V1.0
+	 *//*
+
+	@Override
+	public boolean setMap(String redisKey, final Map<String, String> args) {
+		boolean rv = false;
+
+		try {
+			if (StringUtils.isEmpty(redisKey)) {
+				return false;
+			} else {
+				final String finalKey;
+				if (redisKey instanceof String) {
+					finalKey = redisKey;
+				} else {
+					finalKey = redisKey.toString();
+				}
+				HashOperations<String, String, String> hash = redisTemplate.opsForHash();
+				hash.putAll(finalKey, args);
+			}
+
+		} catch(Exception e) {
+			logger.error("异常：setMap()方法出现异常，异常详细信息：" + e.getMessage() + "。");
+			throw new AppDaoException("redis设置Map出现异常！", e);
+		}
+
+		return rv;
+	}
+
+
+
+	*//**
+	 * 描述： 从redis缓存中查询Map
+	 * <p/>
+	 * 创建时间：2017-03-23
+	 * <p/>
+	 * 创建人： 董耀明
+	 * <p/>
+	 * 修改人：无
+	 * <p/>
+	 * 修改时间：无
+	 * <p/>
+	 * 修改备注：无
+	 * <p/>
+	 * 版本：V1.0
+	 *//*
+
+	@Override
+	public Map<String, String> getMap(String redisKey) {
+		Map<String, String> result = null;
+		try {
+			if (StringUtils.isEmpty(redisKey)) {
+				return null;
+			} else {
+
+				HashOperations<String, String, String> hash = redisTemplate.opsForHash();
+				result = hash.entries(redisKey);
+			}
+		} catch(Exception e) {
+			logger.error("异常：getMap()方法出现异常，异常详细信息：" + e.getMessage() + "。");
+			throw new AppDaoException("从redis缓存中查询Map出现异常！", e);
+		}
+		return result;
+	}
+
+
+
+	*//**
+	 * 描述： 获取redis中mapKey对应value
+	 * <p/>
+	 * 创建时间：2017-03-23
+	 * <p/>
+	 * 创建人： 董耀明
+	 * <p/>
+	 * 修改人：无
+	 * <p/>
+	 * 修改时间：无
+	 * <p/>
+	 * 修改备注：无
+	 * <p/>
+	 * 版本：V1.0
+	 *//*
+
+	@Override
+	public String getFromMap(String redisKey, String mapKey) {
+		String result = null;
+		try {
+			if (StringUtils.isEmpty(redisKey)) {
+				return null;
+			} else {
+
+				HashOperations<String, String, String> hash = redisTemplate.opsForHash();
+				result = hash.get(redisKey, mapKey);
+			}
+		} catch(Exception e) {
+			logger.error("异常：getFromMap()方法出现异常，异常详细信息：" + e.getMessage() + "。");
+			throw new AppDaoException("获取redis中mapKey对应value出现异常！", e);
+		}
+		return result;
+	}
+
+
+
+	*//**
+	 * 描述： 向map中存放key-value
+	 * <p/>
+	 * 创建时间：2017-03-23
+	 * <p/>
+	 * 创建人： 董耀明
+	 * <p/>
+	 * 修改人：无
+	 * <p/>
+	 * 修改时间：无
+	 * <p/>
+	 * 修改备注：无
+	 * <p/>
+	 * 版本：V1.0
+	 *//*
+
+	@Override
+	public void putToMap(String redisKey, String key, String value) {
+		try {
+			if (!StringUtils.isEmpty(redisKey)) {
+				HashOperations<String, String, String> hash = redisTemplate.opsForHash();
+				hash.put(redisKey, key, value);
+			}
+		} catch(Exception e) {
+			logger.error("异常：putToMap()方法出现异常，异常详细信息：" + e.getMessage() + "。");
+			throw new AppDaoException("向map中存放key-value内容出现异常！", e);
+		}
+	}
+
+
+
+
+	public RedisTemplate getRedisTemplate() {
+		return redisTemplate;
+	}
+
+
+
+	public void setRedisTemplate(RedisTemplate redisTemplate) {
+		this.redisTemplate = redisTemplate;
+	}
+
+}
+
+
+*/
