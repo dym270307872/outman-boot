f9f7862372b7e4d99e4a846186067c6bbd6a1075
dym270307872 <270307872@qq.com>
2019-06-05 11:56:33 +0800

    调整目录机构

--- rpc-client/src/main/java/cn/dyaoming/outman/rpc/servers/BaseWebServiceClient.java
+++ rpc-client/src/main/java/cn/dyaoming/outman/rpc/servers/BaseWebServiceClient.java
@@ -15,15 +15,15 @@ public class BaseWebServiceClient {
 		JaxWsDynamicClientFactory dcf = JaxWsDynamicClientFactory.newInstance();
 
 		Client client = dcf
-				.createClient("http://localhost:7001/QYPT_SI/services/WsSSNSP_SIService?wsdl");
+				.createClient("http://localhost:7001/webService?wsdl");
 
 		// 需要密码的情况需要加上用户名和密码
 		// client.getOutInterceptors().add(new ClientLoginInterceptor(USER_NAME, PASS_WORD));
 		Object[] objects = new Object[0];
 		try {
 			// invoke("方法名",参数1,参数2,参数3....);
-			objects = client.invoke("wsSSNSP_SI999",
-					"88888-88888-88888-88888^50001^^^^^^^^^41169940090586||^");
+			objects = client.invoke("wsSSN999",
+					"88888-80586||^");
 			System.out.println("返回数据:" + objects[0]);
 		} catch(java.lang.Exception e) {
 			e.printStackTrace();
