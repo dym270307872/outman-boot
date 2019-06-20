4032eca39f02c840ce27b0a2f73f9a7076608ed2
杀神吴起 <33739022+dym270307872@users.noreply.github.com>
2019-06-20 09:27:57 +0800

    创建项目结构

---
+++ advertising-platform/apApi/src/main/java/cn/dyaoming/leader/apapi/ApiApplication.java
@@ -0,0 +1,17 @@
+package cn.dyaoming.leader.apapi;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class ApiApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(ApiApplication.class, args);
+
+    }
+
+}
