ba11253a38b70fcf2b94b10f2d05810be45f3f82
杀神吴起 <33739022+dym270307872@users.noreply.github.com>
2019-07-10 18:15:28 +0800

    测试连接数据库

--- wechatmall/src/main/java/cn/dyaoming/privatelife/wechatmall/controllers/DemoController.java
+++ wechatmall/src/main/java/cn/dyaoming/privatelife/wechatmall/controllers/DemoController.java
@@ -3,6 +3,8 @@ package cn.dyaoming.privatelife.wechatmall.controllers;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import cn.dyaoming.models.ApiResult;
+
 
 @RestController
 @RequestMapping("/demo")
@@ -11,5 +13,13 @@ public class DemoController {
     @RequestMapping(value = "/main",method = RequestMethod.GET)
     public String main(String name){
         return "hello "+ name;
+    }
+
+
+    public ApiResult Test(){
+
+
+
+        return new ApiResult();
     }
 }
