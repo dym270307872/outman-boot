c5f91564285421a060be03a67fde186fef7be63b
dym270307872 <270307872@qq.com>
2019-09-29 09:37:20 +0800

    提交redis多方式实现测试代码。

---
+++ boot-redis/boot-redisTemplate/src/main/java/cn/dyaoming/boot/config/Config.java
@@ -0,0 +1,11 @@
+package cn.dyaoming.boot.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
+
+@Configuration
+@ImportResource(locations= {"classpath:application-cache.xml"})
+public class Config {
+
+}
+ 
