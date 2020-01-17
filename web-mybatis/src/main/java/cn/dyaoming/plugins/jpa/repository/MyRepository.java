4d6c6f723416fc51acc7d91cd4b6c8c8a7d85cde
杀神吴起 <33739022+dym270307872@users.noreply.github.com>
2020-01-17 11:31:33 +0800

    提交代码

--- web-mybatis/src/main/java/cn/dyaoming/plugins/jpa/repository/MyRepository.java
+++
@@ -1,9 +0,0 @@
-package cn.dyaoming.plugins.jpa.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-
-import java.io.Serializable;
-
-public interface MyRepository<T, ID extends Serializable>  extends JpaRepository<T, ID>, JpaSpecificationExecutor<T> {
-}
