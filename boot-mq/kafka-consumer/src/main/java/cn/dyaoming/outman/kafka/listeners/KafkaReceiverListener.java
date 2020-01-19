f55df6953f3ea7f5c1f5a5661caa451c71c97f24
杀神吴起 <33739022+dym270307872@users.noreply.github.com>
2020-01-19 18:11:35 +0800

    新增kafka示例程序，欠缺广播模式。

---
+++ boot-mq/kafka-consumer/src/main/java/cn/dyaoming/outman/kafka/listeners/KafkaReceiverListener.java
@@ -0,0 +1,27 @@
+package cn.dyaoming.outman.kafka.listeners;
+
+import java.util.Optional;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class KafkaReceiverListener {
+    private final Logger log = LoggerFactory.getLogger(getClass());
+    
+    @KafkaListener(topics = {"zhisheng"})
+    public void listen(ConsumerRecord<?, ?> record) {
+        Optional<?> kafkaMessage = Optional.ofNullable(record.value());
+        if (kafkaMessage.isPresent()) {
+
+            Object message = kafkaMessage.get();
+
+            log.info("----------------- record =" + record);
+            log.info("------------------ message =" + message);
+        }
+
+    }
+}
---
+++ boot-mq/kafka-producer/src/main/java/cn/dyaoming/outman/kafka/servers/KafkaSender.java
@@ -0,0 +1,36 @@
+package cn.dyaoming.outman.kafka.servers;
+
+import java.util.Date;
+import java.util.UUID;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import cn.dyaoming.outman.kafka.model.Message;
+
+@Component
+public class KafkaSender {
+    
+    private final Logger log = LoggerFactory.getLogger(getClass());
+    
+    @Autowired
+    private KafkaTemplate<String, String> kafkaTemplate;
+
+    private Gson gson = new GsonBuilder().create();
+
+    //发送消息方法
+    public void send() {
+        Message message = new Message();
+        message.setId(System.currentTimeMillis());
+        message.setMsg(UUID.randomUUID().toString());
+        message.setSendTime(new Date());
+        log.info("+++++++++++++++++++++  message = {}", gson.toJson(message));
+        kafkaTemplate.send("zhisheng", gson.toJson(message));
+    }
+}
