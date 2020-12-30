ee1668e1d1e4626e54a0512e28a2d9d8a7d39fcc
独醒侠 <270307872@qq.com>
2020-12-30 09:07:56 +0800

    测试字符集对缓存的影响

--- boot-redis/boot-jedis/src/test/java/cn/dyaoming/outman/test/JedisRun.java
+++ boot-redis/boot-jedis/src/test/java/cn/dyaoming/outman/test/JedisRun.java
@@ -1,6 +1,9 @@
 package cn.dyaoming.outman.test;
 
 import junit.BaseJunit;
+
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import redis.clients.jedis.Jedis;
@@ -8,20 +11,24 @@ import redis.clients.jedis.JedisPool;
 
 public class JedisRun extends BaseJunit {
 
-    @Autowired
-    private JedisPool jedisPool;
+	@Autowired
+	private JedisPool jedisPool;
 
-    @Test
-    public void testJedis(){
+	@Test
+	public void testJedis() {
 
-       Jedis jedis = jedisPool.getResource();
-
-        System.out.println(jedis.exists("hello"));
-        System.out.println(jedis.set("hello","hello"));
-        System.out.println(jedis.exists("hello"));
-        System.out.println(jedis.setnx("hello","hello1"));
-        System.out.println(jedis.exists("hello"));
-        System.out.println(jedis.get("hello"));
-
-    }
+		Jedis jedis = jedisPool.getResource();
+		try {
+			jedis.flushDB();
+			System.out.println(jedis.exists("hello董耀明"));
+			System.out.println(jedis.set("hello董耀明".getBytes(), "hello".getBytes()));
+			System.out.println(jedis.exists("hello董耀明"));
+			System.out.println(jedis.setnx("hello董耀明".getBytes("utf-8"), "hello1".getBytes("utf-8")));
+			System.out.println(jedis.exists("hello董耀明".getBytes("GBK")));
+			System.out.println(jedis.get("hello董耀明".getBytes("utf-8")));
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+		
+	}
 }
